3effb6585b7e5198d7eed2bcfa87077bb0a73479
SungKihun <tjdrlgns3293@gmail.com>
2021-10-07 23:17:52 +0900

    단항연산 추가

--- Calculator/CalculatorBrain.swift
+++ Calculator/CalculatorBrain.swift
@@ -18,13 +18,13 @@ class CalculatorBrain {
     var operations: Dictionary<String, Operation> = [
         "π": Operation.Constant(.pi), // .pi,
         "e": Operation.Constant(M_E), //M_E,
-        "√": Operation.UnaryOperation, //sqrt(),
-        "cos": Operation.UnaryOperation // cos()
+        "√": Operation.UnaryOperation(sqrt), //sqrt(),
+        "cos": Operation.UnaryOperation(cos) // cos()
     ]
     
     enum Operation {
         case Constant(Double)
-        case UnaryOperation
+        case UnaryOperation((Double) -> Double)
         case BinaryOperation
         case Equals
     }
@@ -33,7 +33,7 @@ class CalculatorBrain {
         if let operation = operations[symbol] {
             switch operation {
             case .Constant(let value): accumulator = value
-            case .UnaryOperation: break
+            case .UnaryOperation(let function): accumulator = function(accumulator)
             case .BinaryOperation: break
             case .Equals: break
             }
